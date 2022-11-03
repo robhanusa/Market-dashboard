ebf06f5790db9862082025e8d2aa9d91c59654d7
Rob <rkhanusa@gmail.com>
2022-11-03 05:53:09 +0100

    improved graph with dotted line of current value

--- main.py
+++ main.py
@@ -5,23 +5,8 @@ Created on Tue Nov  1 08:33:05 2022
 @author: rkhan
 """
 
-# import requests
 import pandas as pd
-import matplotlib.pyplot as pyplot
-
-# ticker = 'aapl'
-
-# api = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
-# res = requests.get(api)
-
-# res_json = res.json()
-
-# df = pd.DataFrame(res_json['Time Series (5min)']).transpose()
-
-# df.index = pd.to_datetime(df.index)
-# df['4. close'] = pd.to_numeric(df['4. close'])
-
-# pyplot.plot(df.index, df['4. close'])
+import matplotlib.pyplot as plt
 
 from yahoo_fin import stock_info as si
 
@@ -32,4 +17,5 @@ data = si.get_data(ticker, start_date = "01/01/2010")
 
 tickers = si.tickers_sp500()
 
-pyplot.plot(data.index,data['close'])
+plt.plot(data.index,data['close'])
+plt.plot(data.index,[live_price for x in data.index])
