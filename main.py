12cd6c8586af271f45d46e8cf8236cfe9b7246ed
Rob <rkhanusa@gmail.com>
2022-11-07 07:12:56 +0100

    made subplots for different tickers. Need to better format font/legend size.

--- main.py
+++ main.py
@@ -7,15 +7,31 @@ Created on Tue Nov  1 08:33:05 2022
 
 import pandas as pd
 import matplotlib.pyplot as plt
-
 from yahoo_fin import stock_info as si
 
+tickers = ['VOO','VWO','VGK','VTWO']
+labels = ['S&P 500', 'Emerging Markets ETF', 'Europe ETF', 'Russell 2000']
 ticker = 'VOO'
 live_price =  si.get_live_price(ticker)
-table = si.get_quote_table(ticker)
 data = si.get_data(ticker, start_date = "01/01/2010")
 
-tickers = si.tickers_sp500()
+#table = si.get_quote_table(ticker)
+#tickers_sp500 = si.tickers_sp500()
 
-plt.plot(data.index,data['close'])
-plt.plot(data.index,[live_price for x in data.index])
+def ETF_info(ticker):
+    live_price =  si.get_live_price(ticker)
+    data = si.get_data(ticker, start_date = "01/01/2010")
+    return live_price, data
+
+def gen_plot(ticker, label, ax):
+    live_price, data = ETF_info(ticker)
+    ax.plot(data.index,data['close'], label=label)
+    ax.plot(data.index,[live_price for x in data.index], label='Current Price', 
+         linestyle='--')
+    ax.legend()
+
+fig, axs = plt.subplots(2,2, sharex = True)
+gen_plot(tickers[0], labels[0], axs[0,0])
+gen_plot(tickers[1], labels[1], axs[0,1])
+gen_plot(tickers[2], labels[2], axs[1,0])
+gen_plot(tickers[3], labels[3], axs[1,1])
